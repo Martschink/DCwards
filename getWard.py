94f90707f3368b84becd7b0287e6a940829b34dd
Martschink <treemartschink@gmail.com>
2014-04-11 15:51:45 -0400

    updates to make it actually work
    Because it sucked before.

--- getWard.py
+++ getWard.py
@@ -1,6 +1,9 @@
-#Return a ward from input using these resources:
-#http://citizenatlas.dc.gov/newwebservices/locationverifier.asmx?op=findLocation
-#http://octo.dc.gov/node/718362
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Return a ward from input using these resources:
+# http://citizenatlas.dc.gov/newwebservices/locationverifier.asmx?op=findLocation
+# http://octo.dc.gov/node/718362
 
 
 
@@ -10,6 +13,13 @@ def findWard():
 	addy=raw_input("What is you address?")
 	r=requests.get(url+addy)
 	ward=r.text.split("<WARD>")[1].split("</WARD>")[0]
-	
 
-findWard()
+	print ward
+
+def main():
+	findWard()
+
+
+
+if __name__ == '__main__':
+	main()
