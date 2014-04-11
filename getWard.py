0c4789b165fa2fe4ecc93676aef1edbf6a9fa869
Martschink <treemartschink@gmail.com>
2014-04-11 15:38:33 -0400

    a sloppy first commit

---
+++ getWard.py
@@ -0,0 +1,15 @@
+#Return a ward from input using these resources:
+#http://citizenatlas.dc.gov/newwebservices/locationverifier.asmx?op=findLocation
+#http://octo.dc.gov/node/718362
+
+
+
+def findWard():
+	import requests
+	url="http://citizenatlas.dc.gov/newwebservices/locationverifier.asmx/findLocation?str="
+	addy=raw_input("What is you address?")
+	r=requests.get(url+addy)
+	ward=r.text.split("<WARD>")[1].split("</WARD>")[0]
+	
+
+findWard()
